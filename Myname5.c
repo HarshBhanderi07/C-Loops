6c4176838ecb4c4bd19c9d24fdf04ad184ee7f33
HarshBhanderi07 <harshbhanderijkn@gmail.com>
2025-09-08 09:03:00 +0530

    Add files via upload

---
+++ Myname5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i;
+    for(i = 1; i <= 5; i++) 
+    {
+        printf("Harsh Bhanderi\n");
+    }
+    return 0;
+}
---
+++ Mynamen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n, i;
+    printf("Enter the number of times to print your name: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) 
+    {
+        printf("Harsh Bhanderi\n");
+    }
+    return 0;
+}
